825a8628f78b46772d08a94070215da02d57fae6
jasoncodings-bit <jasoncodings@gmail.com>
2026-06-03 20:27:13 +0800

    Add files via upload

---
+++ Scripts/Edit.js
@@ -0,0 +1,5 @@
+// Make Everything Editable
+(function() {
+  document.designMode = document.designMode === 'on' ? 'off' : 'on';
+  console.log('[ScriptForge] Design mode: ' + document.designMode);
+})();
