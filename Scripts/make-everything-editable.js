1e15c5b40cc3361a7f97d7b7d9991081962d3718
jasoncodings-bit <jasoncodings@gmail.com>
2026-06-03 20:41:06 +0800

    Delete Scripts/make-everything-editable.js

--- Scripts/make-everything-editable.js
+++
@@ -1,5 +0,0 @@
-// Make Everything Editable
-(function() {
-  document.designMode = document.designMode === 'on' ? 'off' : 'on';
-  console.log('[ScriptForge] Design mode: ' + document.designMode);
-})();
